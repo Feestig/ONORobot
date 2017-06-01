113325cd9f300f2f65c2a9427be185fc662f0213
ArnoVDC <arnovdc@msn.com>
2017-06-01 00:33:22 -0700

    changed api keys

--- src/opsoro/apps/sociono/__init__.py
+++ src/opsoro/apps/sociono/__init__.py
@@ -59,14 +59,14 @@ def setup_pages(server):
 
     server.register_app_blueprint(app_bp)
 
-access_token = '735437381696905216-BboISY7Qcqd1noMDY61zN75CdGT0OSc'
-access_token_secret = 'd3A8D1ttrCxYV76pBOB389YqoLB32LiE0RVyoFwuMKUMb'
-consumer_key = 'AcdgqgujzF06JF6zWrfwFeUfF'
-consumer_secret = 'ss0wVcBTFAT6nR6hXrqyyOcFOhpa2sNW4cIap9JOoepcch93ky'
+access_token = '141268248-yAGsPydKTDgkCcV0RZTPc5Ff7FGE41yk5AWF1dtN'
+access_token_secret = 'UalduP04BS4X3ycgBJKn2QJymMhJUbNfQZlEiCZZezW6V'
+consumer_key = 'tNYqa3yLHTGhBvGNblUHHerlJ'
+consumer_secret = 'NxBbCA8VJZvxk1SNKWw3CWd5oSnJyNAcH9Kns5Lv1DV0cqrQiz'
 
 
 
-twitterWords = ['#tennis']
+twitterWords = ['#opsoro']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
