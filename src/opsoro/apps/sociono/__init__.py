be45addebd2a91494a1a1c1aeabf1295ec600dcf
ArnoVDC <arnovdc@msn.com>
2017-06-02 02:22:11 -0700

    Socket connection to js

---
+++ src/opsoro/apps/sociono/__init__.py
@@ -0,0 +1,161 @@
+from __future__ import with_statement
+
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_from_directory
+from werkzeug import secure_filename
+from opsoro.sound import Sound
+
+import math
+import cmath
+
+from opsoro.robot import Robot
+from opsoro.console_msg import *
+from opsoro.hardware import Hardware
+from opsoro.stoppable_thread import StoppableThread
+
+from functools import partial
+from exceptions import RuntimeError
+import os
+import glob
+import shutil
+import time
+import yaml
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+
+import tweepy
+
+constrain = lambda n, minn, maxn: max(min(maxn, n), minn)
+
+# from opsoro.expression import Expression
+
+config = {
+    'full_name':            'Sociono',
+    'icon':                 'fa-info',
+    'color':                'green',
+    'difficulty':           4,
+    'tags':                 [''],
+    'allowed_background':   False,
+    'connection':           Robot.Connection.OFFLINE,
+    'activation':           Robot.Activation.AUTO
+}
+config['formatted_name'] =  config['full_name'].lower().replace(' ', '_')
+
+
+get_path = partial(os.path.join, os.path.abspath(os.path.dirname(__file__)))
+
+dof_positions = {}
+
+clientconn = None
+
+def send_stopped():
+    global clientconn
+    if clientconn:
+        clientconn.send_data('soundStopped', {})
+
+
+def SocionoRun():
+    Sound.wait_for_sound()
+    send_stopped()
+
+
+sociono_t = None
+
+
+def setup_pages(opsoroapp):
+    sociono_bp = Blueprint(
+        config['formatted_name'],
+        __name__,
+        template_folder='templates',
+        static_folder='static')
+
+    @sociono_bp.route('/', methods=['GET', 'POST'])
+    @opsoroapp.app_view
+    def index():
+        data = {'actions': {}, 'emotions': [], 'sounds': []}
+
+        action = request.args.get('action', None)
+        if action != None:
+            data['actions'][action] = request.args.get('param', None)
+
+        with open(get_path('emotions.yaml')) as f:
+            data['emotions'] = yaml.load(f, Loader=Loader)
+
+        filenames = glob.glob(get_path('../../data/sounds/*.wav'))
+
+        for filename in filenames:
+            data['sounds'].append(os.path.split(filename)[1])
+        data['sounds'].sort()
+
+        # Auguste code
+        if request.method == "POST":
+            stopTwitter()
+            social_id = []
+            social_id.append(request.form['social_id'])
+            startTwitter(social_id)
+
+        return opsoroapp.render_template(config['formatted_name'] + '.html', **data)
+
+    @opsoroapp.app_socket_connected
+    def s_connected(conn):
+        global clientconn
+        clientconn = conn
+
+    @opsoroapp.app_socket_disconnected
+    def s_disconnected(conn):
+        global clientconn
+        clientconn = None
+
+
+    opsoroapp.register_app_blueprint(sociono_bp)
+
+
+access_token = '141268248-yAGsPydKTDgkCcV0RZTPc5Ff7FGE41yk5AWF1dtN'
+access_token_secret = 'UalduP04BS4X3ycgBJKn2QJymMhJUbNfQZlEiCZZezW6V'
+consumer_key = 'tNYqa3yLHTGhBvGNblUHHerlJ'
+consumer_secret = 'NxBbCA8VJZvxk1SNKWw3CWd5oSnJyNAcH9Kns5Lv1DV0cqrQiz'
+
+#twitterWords = ['#opsoro']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+#getting new tweet
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print_info(status.text)
+        #print_info(status._json)
+        #stopTwitter()
+        #stop()
+        #Sound.say_tts(status.text)
+        #TODO send socket to js
+        clientconn.send_data("this is data", {})
+
+api = tweepy.API(auth)
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+
+# Default functions for setting up, starting and stopping an app
+def setup(opsoroapp):
+    pass
+
+def start(opsoroapp):
+    pass
+
+def stop(opsoroapp):
+    pass
+
+def startTwitter(twitterWords):
+    global myStream
+    myStream.filter(track=twitterWords, async=True)
+
+    print_info(twitterWords)
+
+def stopTwitter():
+    global myStream
+    myStream.disconnect()
+
+    print_info("stop twitter stream")
--- src/opsoro/apps/sociono/static/app.js
+++ src/opsoro/apps/sociono/static/app.js
@@ -1,88 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-    var Model = function() {
-        var self = this;
-
-        // File operations toolbar item
-        self.fileIsLocked = ko.observable(false);
-        self.fileIsModified = ko.observable(false);
-        self.fileName = ko.observable("");
-        self.fileStatus = ko.observable("");
-        self.fileExtension = ko.observable(".ext");
-
-        // Script operations toolbar item
-        self.isRunning = ko.observable(false);
-        self.isUI = ko.observable(false);
-
-        // Lock/Unlock toolbar item
-        self.toggleLocked = function() {
-            if (self.fileIsLocked()) {
-                self.unlockFile();
-            } else {
-                self.lockFile();
-            }
-        };
-        self.lockFile = function() {
-            self.fileIsLocked(true);
-            self.fileStatus("Locked")
-        };
-        self.unlockFile = function() {
-            self.fileIsLocked(false);
-            self.fileStatus("Editing")
-        };
-
-        // Popup window
-        self.popupTextInput = ko.observable("Hi! This text can be changed. Click on the button to change me!");
-        self.showPopup = function() {
-          $("#popup_window").foundation('open');
-
-        };
-        self.closePopup = function() {
-            $("#popup_window").foundation('close');
-        };
-        self.popupButtonHandler = function() {
-            self.closePopup();
-        };
-
-        self.init = function() {
-            // Clear data, new file, ...
-            self.fileName("Untitled");
-            self.unlockFile();
-            self.fileIsModified(false);
-        };
-
-        self.loadFileData = function(data) {
-            if (data == undefined) {
-                return;
-            }
-
-            // Load data, parse if needed
-            var dataobj = JSON.parse(data);
-
-
-            self.fileIsModified(false);
-            self.lockFile();
-        };
-
-        self.saveFileData = function() {
-            // Convert data
-            file_data = {};
-
-            var data = ko.toJSON(file_data, null, 2);
-            self.fileIsModified(false);
-            return data;
-        };
-    };
-    // This makes Knockout get to work
-    var model = new Model();
-    ko.applyBindings(model);
-    model.fileIsModified(false);
-
-  
-    // Configurate toolbar handlers
-    //config_file_operations("", model.fileExtension(), model.saveFileData, model.loadFileData, model.init);
-=======
-
 $(document).ready(function(){
 
 	ko.bindingHandlers.avatar = {
@@ -414,7 +329,7 @@ $(document).ready(function(){
 		self.socialID = ko.observable("");
 
 		self.setSocialID = function() {
-			
+
 			console.log("Hello world");
 			console.log(socialID.value)
 
@@ -423,16 +338,36 @@ $(document).ready(function(){
 			})
 		}
 
+
+		self.conn = new SockJS("http://" + window.location.host + "/appsockjs");
+
+		self.conn.onopen = function(){
+			$.ajax({
+				url: "/appsockjstoken",
+				cache: false
+			})
+			.done(function(data) {
+				self.conn.send(JSON.stringify({action: "authenticate", token: data}));
+				self.connReady = true;
+			});
+		};
+
+		self.conn.onmessage = function(e){
+			var msg = $.parseJSON(e.data);
+			console.log(msg);
+			console.log(e);
+			//TODO verwerk msg
+
+
+		};
+
+
+
 	};
 	// This makes Knockout get to work
 	var model = new SocialScriptModel();
 	ko.applyBindings(model);
 	model.fileIsModified(false);
 
-
-
-
 	config_file_operations("scripts", model.fileExtension(), model.saveFileData, model.loadFileData, model.init);
-
->>>>>>> master
 });
