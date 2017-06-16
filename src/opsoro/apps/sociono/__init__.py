805778984dff04dd462ba83339f0ab744e043ca2
ArnoVDC <arnovdc@msn.com>
2017-06-16 02:17:24 -0700

    bug fixing

--- src/opsoro/apps/sociono/__init__.py
+++ src/opsoro/apps/sociono/__init__.py
@@ -64,8 +64,8 @@ Emoticons = []
 
 access_token = '141268248-yAGsPydKTDgkCcV0RZTPc5Ff7FGE41yk5AWF1dtN'
 access_token_secret = 'UalduP04BS4X3ycgBJKn2QJymMhJUbNfQZlEiCZZezW6V'
-consumer_key = 'tNYqa3yLHTGhBvGNblUHHerlJ'
-consumer_secret = 'NxBbCA8VJZvxk1SNKWw3CWd5oSnJyNAcH9Kns5Lv1DV0cqrQiz'
+consumer_key = 'jHW1X0K58pKhPePYDVk1ko2E6'
+consumer_secret = '5SzUVSd0DD2aMPqwNRyGuS1ovWnEb4qSaMnPkAHZSKu5G42pri'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -132,7 +132,7 @@ def setup_pages(opsoroapp):
             loop_T = StoppableThread(target=wait_for_sound)
 
         if request.form['action'] == 'autoLoopTweepyStop':
-            global autolooping
+            #global autolooping
             autolooping = 0
             send_action(request.form['action'])
 
@@ -140,6 +140,9 @@ def setup_pages(opsoroapp):
             if request.form['data']:
                 tweepyObj = json.loads(request.form['data'])
                 playTweet(tweepyObj)
+        if request.form['action'] == 'toggleAutoRead':
+            autoRead = not autoRead
+            print_info(autoRead)
 
         return opsoroapp.render_template(config['formatted_name'] + '.html', **data)
 
